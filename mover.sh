fb90cea9ad50f90b73b5380be279b464bd6b92f7
Santiago <fernandezsantid@gmail.com>
2015-04-17 14:56:10 -0300

    Casi terminado el comando mover.sh, esta la mayor parte funcionando. Lo unico que faltaria es ver lo de la secuencia por el tema de cuando hay duplicados. Por ahora puse que se agregue la fecha hasta que sepa como hacer lo que pide en el enunciado.

--- mover.sh
+++ mover.sh
@@ -1,4 +1,4 @@
-#!bin/bash
+#!/bin/bash
 
 # Script a cargo del movimiento de archivos entre directorios
 #-----------------------------------------------------------------------------------------------------------
@@ -20,4 +20,83 @@
 #  - Si hay otro tipo de problema siempre se va a logear en el log del comando que ejecuto la funcion mover, en
 #		caso de que no se pase el nombre del comando, por defecto se escribe en el log de mover.
 #------------------------------------------------------------------------------------------------------------
+if [ $# -eq 3 ] 		#Si hay 3 parametros significa que se ingreso el nombre del comando.
+then
 
+	COMANDO=$3			#Se asigna el nombre del comando con la variable.
+
+elif [ $# -eq 2 ]		#Si hay 2 parametros solo se ingreso el origen y el destino.
+then
+
+	COMANDO="Mover"		#En este caso los errores van al log del Mover.
+
+else
+
+	# Si no hay ni 2 o 3 parametros significa que hay un error: "Parametros invalidos"
+	$BINDIR/glog.sh "Mover" "Recibidos $# parametros en lugar de 2 o 3" "ERR"
+	exit -1
+
+fi
+
+#Se asignan los datos ingresados con las variables.
+ORIGEN=$1
+DESTINO=$2
+
+if [ "$ORIGEN" == "$DESTINO" ] 	#Se verifica si el directorio origen y destino son iguales.
+then
+
+	#Si el origen y el destino son iguales, no mover y registrar en el log el error
+	$BINDIR/glog.sh "$COMANDO" "El Origen y el Destino son iguales" "ERR"
+	exit -2
+
+fi
+
+NOMBRE_ARCHIVO="${ORIGEN##*/}"
+_DUPDIR="${DUPDIR##*/}"
+
+if [ ! -f "$ORIGEN" ] 		#Verifica si existe el archivo en el origen.
+then
+
+	# Si el origen no existe, no mover y registrar en el log el error
+	$BINDIR/glog.sh "$COMANDO" "No existe el archivo origen que se quiere mover" "ERR"
+	exit -3 
+
+elif [ ! -d "$DESTINO" ]	#Verifica si existe el directorio destino.
+then
+
+	# Si el destino no existe, no mover y registrar en el log el error
+	$BINDIR/glog.sh "$COMANDO" "No existe el destino al que se quiere mover el archivo" "ERR"
+	exit -4 
+
+else
+
+	#Me fijo si ya existe en el destino un archivo con el mismo nombre del archivo que se quiere mover.
+	OCURRENCIA=$(ls "$DESTINO" | grep -c "${NOMBRE_ARCHIVO}")
+
+	#Si hay alguna ocurrencia.
+	if [ "$OCURRENCIA" -ne 0 ]
+	then
+        
+        #Si no existe el directorio de DUPDIR en el destino.
+		if [ ! -d "${DESTINO}/$_DUPDIR" ]
+		then
+
+			mkdir "${DESTINO}/$_DUPDIR" 	#Entonces lo creo.
+
+		fi
+
+		#TODO: FALTA MODIFICAR ESTO DE LA SECUENCIA, DEBERIA SER .NNN Y NO LA FECHA
+		#Uso la fecha ahora solo como prueba hasta ver bien lo de la SECUENCIA.
+		SECUENCIA="$(date '+%Y-%m-%d-%H:%M:%S')"
+
+		#Muevo el archivo duplicado al directorio DUPDIR con un cambio de nombre que se le agrega una secuencia.
+		mv "${DESTINO}/$NOMBRE_ARCHIVO" "${DESTINO}/$_DUPDIR/${NOMBRE_ARCHIVO}.$SECUENCIA"
+
+	fi
+
+	#Si no esta repetido el archivo en el directorio destino muevo el archivo.
+	mv "$ORIGEN" "$DESTINO"
+	
+	exit 0
+
+fi
