5351227d11ac439d2c3264458088a11d55723e14
Maria <maria.manzione@gmail.com>
2015-04-26 02:22:23 -0300

    Inicializa variables de ambiente harcodeadas por ahora

---
+++ IniPro.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+# Script a cargo de la inicializacion de variables de ambiente
+#-----------------------------------------------------------------------------------------------------------
+# ¿Como usar el script?
+# >$ ./IniPro.sh 
+#------------------------------------------------------------------------------------------------------------
+
+#export PATH=
+export GRUPO="$PWD/grupo02"
+export NOVEDIR="$GRUPO/novedades"
+export RECHDIR="$GRUPO/rechazados"
+export BINDIR="$GRUPO/bin"
+export MAEDIR="$GRUPO/mae"
+export REPODIR="$GRUPO/informes"
