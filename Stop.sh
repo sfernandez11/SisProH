94de95023896814b0202709fe1ef659715363764
mmanzione <maria.manzione@keepcon.com>
2015-04-29 15:44:16 -0300

    Nuevos Start y Stop. Se le pasa por parametro RecPro

--- Stop.sh
+++ Stop.sh
@@ -3,18 +3,22 @@
 # Script a cargo parar la ejecución del demonio
 #-----------------------------------------------------------------------------------------------------------
 # Ejecutar de esta forma:
-# >$ Stop.sh
+# >$ Stop.sh RecPro
 #------------------------------------------------------------------------------------------------------------
 
-PID=`pidof RecPro.sh`
+
+PID=`ps aux | grep $BINDIR/$1.sh | grep -v grep | awk '{print $2}'`
 
 if [ "$PID" != "" ]; then
 	kill -9 $PID
-	PID=`pidof RecPro.sh`
+
+	PID=`ps aux | grep $BINDIR/$1.sh | grep -v grep | awk '{print $2}'`
+
 	if [ "$PID" != "" ]; then
 	    echo "No se pudo detener el demonio"    
     	exit 1
-    fi
+	fi
+
     echo "Se detuvo el demonio"
     exit 0
 fi
