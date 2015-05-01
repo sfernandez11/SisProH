98f060f4b4850c3afda9876225a370f2bb8f40de
Rodrigo Echeconea <recheconea@gmail.com>
2015-05-01 15:22:53 -0300

    Version preliminar de la consulta

--- InfPro/InfPro.pl
+++ InfPro/InfPro.pl
@@ -2,23 +2,21 @@
 use strict;
 use warnings;
 use Switch;
-use InfProConsulta;
+use 'InfProQuery.pm';
 
-$num_args = $#ARGV + 1;
-if ($num_args == 0) die "InfPro necesita, al menos, un argumento. Ejecute 'InfPro.pl -a' para ver informacion al respecto";
-
-$keyword = $argv[1];
-
-switch ($argv[0]) {
-	case a {}
-	case c {
-		$query = InfProConsulta->new(false, $keyword);
-		$query->runQueryMode();
-	}
-	case i {}
-	case e {}
+my $num_args = $#ARGV + 1;
+if ($num_args == 0) {
+	die "InfPro necesita, al menos, un argumento. Ejecute 'InfPro.pl -a' para ver informacion al respecto";
 }
 
+my $keyword = $ARGV[1];
 
-sub 
-
+switch ($ARGV[0]) {
+	case 'a' {}
+	case 'c' {
+		my $query = InfProQuery->new('', $keyword);
+		$query->runQueryMode();
+	}
+	case 'i' {}
+	case 'e' {}
+}
--- InfPro/InfProConsulta.pm
+++ InfPro/InfProConsulta.pm
@@ -19,33 +19,36 @@ sub runQueryMode {
 	do {
 		showQueryMenu();
 	} until (!isEmptyFilter());
-	$procdir = "PROCDIR/";
+	my $procdir = "PROCDIR/";
+	my @dlist;
 	if (opendir(DIRH,"$procdir")) {
 		@dlist=readdir(DIRH);
 		closedir(DIRH);
 	} else {
 		die("No se pudo abrir el directorio de PROCDIR");
 	}
-	@filesList = [];
+	my @fileList = [];
 	foreach (@dlist) {
 		# ignorar . y .. :
 		next if ($_ eq "." || $_ eq "..");
 		if ( -d "$procdir/$_" ) {
+			my @flist;
 			if (opendir(DIRH, "$procdir/$_")) {
 				@flist=readdir(DIRH);
 				closedir(DIRH);
 			} else {
 				next;
 			}
-			$dir = "$procdir/$_"
+			my $dir = "$procdir/$_";
 			foreach (@flist) {
 				open FILE, "<$dir/$_";
-				$file_contents = do { local $/; <FILE> };
-				push @fileList, $file_contents
+				my $file_contents = paseDoc("$dir/$_");
+				push @fileList, $file_contents;
 			}
 		}
 	}
-	applyFilters(@fileList);
+	@fileList = applyFilters(@fileList);
+	@fileList = sortResults('', @fileList);
 }
 
 sub showQueryMenu {
@@ -72,18 +75,42 @@ sub isEmptyFilter {
 	my $self = shift;
 	my @filters = keys $self->{filters};
 	for my $filter (@filters) {
-	    if ($self->{filters}{$filter} ne '') return false;
+	    if ($self->{filters}{$filter} ne '') {
+	    	return 0;
+	    }
 	}
 	print "\033[2J";    #clear the screen
 	print "\033[0;0H"; #jump to 0,0
 	print "Necesita elegir al menos un filtro.";
-	return true;
+	return 1;
 }
 
 sub parseDoc {
 	my $self = shift;
 	my $file = shift;
-
+	my @fileList = [];
+	open FILE, "$file" or print "No pude abrir el archivo $file";
+	while (my $line = <FILE>) {
+		my @splittedLine = split(';', $line);
+		my %fileParsed = {
+			'fuente' => $splittedLine[0],
+			'fecha_norma' => $splittedLine[1],
+			'nro_norma' => $splittedLine[2],
+			'anio_norma' => $splittedLine[3],
+			'causante' => $splittedLine[4],
+			'extracto' => $splittedLine[5],
+			'cod_tema' => $splittedLine[6],
+			'expedienteId' => $splittedLine[7],
+			'expedienteAnio' => $splittedLine[8],
+			'cod_firma' => $splittedLine[9],
+			'idRegistro' => $splittedLine[10],
+			'cod_gestion' => $splittedLine[11],
+			'cod_norma' => $splittedLine[12],
+			'cod_emisor' => $splittedLine[13],
+		};
+		push @fileList, %fileParsed;
+	}
+	return @fileList;
 }
 
 sub applyFilters {
@@ -91,13 +118,11 @@ sub applyFilters {
 	my @fileList = shift;
 	my @filters = keys $self->{filters};
 	for my $filter (@filters) {
-	    switch ($filter) {
-	    	case 'tNorma' {@fileList = applyTNormaFilter(self->{filters}{filter}, @filters)}
-	    	case 'año' {@fileList = applyAnioFilter(self->{filters}{filter}, @filters)}
-	    	case 'nNorma' {@fileList = applyNNormaFilter(self->{filters}{filter}, @filters)}
-	    	case 'gestion' {@fileList = applyGestionFilter(self->{filters}{filter}, @filters)}
-	    	case 'emisor' {@fileList = applyEmisorFilter(self->{filters}{filter}, @filters)}
-	    }
+		if ($filter eq 'tNorma') {@fileList = applyTNormaFilter($self->{filters}{filter}, @filters);}
+		elsif ($filter eq 'año') {@fileList = applyAnioFilter($self->{filters}{filter}, @filters);}
+		elsif ($filter eq 'nNorma') {@fileList = applyNNormaFilter($self->{filters}{filter}, @filters);}
+		elsif ($filter eq 'gestion') {@fileList = applyGestionFilter($self->{filters}{filter}, @filters);}
+		elsif ($filter eq 'emisor') {@fileList = applyEmisorFilter($self->{filters}{filter}, @filters);}
 	}
 	return @filters;
 }
@@ -106,33 +131,131 @@ sub applyTNormaFilter {
 	my $self = shift;
 	my $filter = shift;
 	my @fileList = shift;
-	if ($filter eq '') return @fileList;
+	if ($filter eq '') {
+		return @fileList;
+	}
+	foreach my $i (0 .. $#fileList) {
+		if (@fileList[$i]->{cod_norma} ne $filter) {
+			splice @fileList, $i, 1;
+		}
+	}
+	return @fileList;
+
 }
 
 sub applyAnioFilter {
 	my $self = shift;
 	my $filter = shift;
 	my @fileList = shift;
-	if ($filter eq '') return @fileList;
+	my @filterSplitted = split $filter, '-';
+	if ($filter eq '') {
+		return @fileList;
+	}
+	foreach my $i (0 .. $#fileList) {
+		if (@fileList[$i]->{anio_norma} < $filterSplitted[0] || @fileList[$i]->{anio_norma} > $filterSplitted[1]) {
+			splice @fileList, $i, 1; 
+		}
+	}
+	return @fileList;
 }
 
 sub applyNNormaFilter {
 	my $self = shift;
 	my $filter = shift;
 	my @fileList = shift;
-	if ($filter eq '') return @fileList;
+	if ($filter eq '') {
+		return @fileList;
+	}
+	my @filterSplitted = split $filter, '-';
+	foreach my $i (0 .. $#fileList) {
+		if (@fileList[$i]->{nro_norma} < $filterSplitted[0] || @fileList[$i]->{nro_norma} > $filterSplitted[1]) {
+			splice @fileList, $i, 1; 
+		}
+	}
+	return @fileList;
 }
 
 sub appliyGestionFilter {
 	my $self = shift;
 	my $filter = shift;
 	my @fileList = shift;
-	if ($filter eq '') return @fileList;
+	if ($filter eq '') {
+		return @fileList;
+	}
+	foreach my $i (0 .. $#fileList) {
+		if (@fileList[$i]->{cod_gestion} ne $filter) {
+			splice @fileList, $i, 1;
+		}
+	}
+	return @fileList;
 }
 
 sub applyEmisorFilter {
 	my $self = shift;
 	my $filter = shift;
 	my @fileList = shift;
-	if ($filter eq '') return @fileList;
+	if ($filter eq '') {
+		return @fileList;
+	}
+	foreach my $i (0 .. $#fileList) {
+		if (@fileList[$i]->{cod_emisor} ne $filter) {
+			splice @fileList, $i, 1;
+		}
+	}
+	return @fileList;
 }
+
+sub sortResults {
+	my $self = shift;
+	my $keyWord = shift;
+	my @fileList = shift;
+	if ($keyWord ne '') {
+		return sortResultsByDate(@fileList);
+	} else {
+		return sortResultsByWeigth(@fileList, $keyWord);
+	}
+}
+
+sub sortResultsByWeigth {
+
+}
+
+sub sortResultsByDate {
+	my $self = shift;
+	my @filesList = shift;
+	foreach my $i (0..$#filesList) {
+		foreach my $j ($i+1 .. $#filesList) {
+			if (formatDate(@filesList[$i]->{fecha_norma}) > formatDate(@filesList[$j]->{fecha_norma})) {
+				swapFiles(@filesList, $i, $j);
+			}
+		}
+	}
+	return @filesList;
+}
+
+sub formatDate {
+	my $date = shift;
+	my $splittedDate = split $date, '-';
+	return join '', reverse $splittedDate;
+}
+
+sub swapFiles {
+	my %aux = {};
+	%aux = $_[0][$_[1]];
+	$_[0][$_[1]] = $_[0][$_[2]];
+	$_[0][$_[2]] = %aux;
+}
+
+sub printResults {
+	my $self = shift;
+	my @fileList = shift;
+	my $i = 1;
+	foreach (@fileList) {
+		print "$i) $_{'cod_norma'} $_{'cod_emisor'} $_{'nro_norma'}/$_{'anio_norma'} $_{'cod_gestion'} $_{'fecha_norma'} peso \n
+			\t $_{'causante'} \n
+			\t $_{'extracto'} \n\n";
+		$i++;
+	}
+}
+
+return 1;
