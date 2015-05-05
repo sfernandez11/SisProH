5bb25c66437cb657c93206e15f0958ae5a00e7b3
Agustín <agscontini@gmail.com>
2015-05-04 22:21:17 -0300

    Limpieza de logs y sumado chequeo de isInteger. Sigue sin persistir bien las tablas el mover.

--- ProPro/ProPro.sh
+++ ProPro/ProPro.sh
@@ -4,10 +4,11 @@ source ProProFunctions.sh
 
 $BINDIR/glog.sh "ProPro" "Inicio de ProPro"
 cantDir=`ls $ACEPDIR | wc -l`
-cantArchivos=0
+cantArchivos=`find $ACEPDIR -type f -exec ls -l {} \; | wc -l`
+$BINDIR/glog.sh "ProPro" "Cantidad de archivos a procesar: $cantArchivos"
 archivosProcesados=0
 archivosRechazados=0
-$BINDIR/glog.sh "ProPro" "Cantidad de carpetas a analizar para procesar: $cantDir"
+#$BINDIR/glog.sh "ProPro" "Cantidad de carpetas a analizar para procesar: $cantDir"
 gestiones=$(cat $MAEDIR/gestiones.mae | sed 's-\([^;]*\);\([^/]*\)/\([^/]*\)/\([^;]*\);\(.*\)-\1;\4\3\2;\5-' |  sort -t";" -nk2 | sed 's/\([^;]*\);\(.*\)/\1/')
 for gest in $gestiones
 do
@@ -22,10 +23,8 @@ do
 	then
 		$BINDIR/glog.sh "ProPro" "No hay archivos para procesar para la gestion: $gest, se sigue."
 		continue
-	else
-		cantArchivos=$(( cantArchivos + $hayArchivos ))
 	fi
-	$BINDIR/glog.sh "ProPro" "Archivos a procesar de $gest: $hayArchivos"
+	#$BINDIR/glog.sh "ProPro" "Archivos a procesar de $gest: $hayArchivos"
 	docs=$(ls $dir | sort -t"_" -k5)
 	for doc in $docs
 	do
@@ -37,7 +36,6 @@ do
 		else
 			normaEmisor=`echo $doc | sed 's/^\([^_]*\)_\([^_]*\)_\([^_]*\)_\(.*\)/\2;\3/'`	
 			hasNorma=`cat $MAEDIR/tab/nxe.tab | grep $normaEmisor`
-			$BINDIR/glog.sh "ProPro" "has norma que devuelve: $hasNorma"
 			if [ -z $hasNorma ]
 			then
 				$BINDIR/glog.sh "ProPro" "Se rechaza el archivo. Emisor no habilitado en este tipo de norma."
@@ -45,15 +43,12 @@ do
 				$BINDIR/mover.sh $dir/$doc $RECHDIR "ProPro"
 				continue
 			fi
-			#$BINDIR/glog.sh "ProPro" "Genero archivo temporal sin lineas vacias, y lo paso para procesar los registros."
-			#sed '/^$' $dir/$doc > $dir/$doc.t
 			DOCTYPE=$(getDocType $doc)
 			archivosProcesados=$(( archivosProcesados + 1 ))
 			writeRecordOutput $dir/$doc $DOCTYPE $doc
-			#$BINDIR/glog.sh "ProPro" "Elimino el archivo temporal sin lineas vacias que use para procesar."
-			#rm  $dir/$doc.t
 			$BINDIR/glog.sh "ProPro" "Muevo el archivo procesado a la carpeta proc"
 			$BINDIR/mover.sh $dir/$doc $PROCDIR/proc "ProPro"
+			continue
 		fi
 	done
 done
